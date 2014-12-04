e1bc098f77065a5b3ab18c0bd32aa93acc021c06
Luca De Vitis <luca@monkeython.com>
2014-12-04 18:58:13 +0100

    Fix module renaming

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,8 +1,8 @@
 import unittest
 
-from tests import test_pluggable_package
+from tests import test_multipla
 
 suite = unittest.TestSuite()
 loader = unittest.defaultTestLoader
-suite.addTest(loader.loadTestsFromModule(test_pluggable_package))
+suite.addTest(loader.loadTestsFromModule(test_multipla))
 
